8de571cb6a3eabc00fe640411b97805592492d9d
yuri <yuri.diogo.az@gmail.com>
2023-04-03 12:18:52 -0300

    Update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,13 +16,13 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#">
+              <a href="https://www.linkedin.com/in/yuri-diogo-az/">
                 <img src={navIcon1} alt="Icon" />
               </a>
-              <a href="#">
+              <a href="https://www.facebook.com/yuri.az.31">
                 <img src={navIcon2} alt="Icon" />
               </a>
-              <a href="#">
+              <a href="https://www.instagram.com/yuri_aaz/">
                 <img src={navIcon3} alt="Icon" />
               </a>
             </div>
